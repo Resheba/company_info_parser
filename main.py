c858faf5ecd823850c6cf255835d93b2c53580be
Resheba <74815401+Resheba@users.noreply.github.com>
2023-09-13 18:25:41 +0300

    except & waiting

--- main.py
+++ main.py
@@ -1,14 +1,28 @@
+import time
+
 from parser.service import ParserSerivce
 from database.service import DataBaseSerivce
+
+
+def processing(inn: int) -> None:
+    if company := ParserSerivce.get_company(inn):
+        DataBaseSerivce.append_company(company)
+    else:
+        print(inn, 'missed')
+
 
 def main():
     inns: tuple[str] = DataBaseSerivce.get_inns()
 
     for inn in inns:
-        if company := ParserSerivce.get_company(inn):
-            DataBaseSerivce.append_company(company)
-        else:
-            print(inn, 'missed')
-    
+        while True:
+            try:
+                processing(inn)
+                print(inn, 'COMPLATE')
+                break
+            except Exception as ex:
+                print(inn, ex)
+                time.sleep(30)
+        
 if __name__ == '__main__':
     main()
